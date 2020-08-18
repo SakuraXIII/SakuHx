502b1072f50c1ed2ead47038fed2f22e9787ff3f
SakuraXIII <1162617171@qq.com>
2020-08-18 12:38:35 +0800

    实现获取文章列表并插入列表

--- theme/NES/index.js
+++ theme/NES/index.js
@@ -2,45 +2,58 @@ const fs = require("fs");
 const { exec } = require("child_process");
 const { shell, remote } = require("electron");
 const { clipboard } = require("electron");
-let global = remote.getGlobal;
-console.log( remote.getGlobal('mySiteInfo '))
+let global = remote.getGlobal("mySiteInfo");
 let aside = document.querySelector("#aside");
 let postList = [];
 
-let active = 1;
+let active = 2;
 
 function init() {
   loadUser();
   loadView();
 }
-
+/**
+ * 初始化界面信息
+ */
 function loadUser() {
-  document.getElementById("userAvator").src = global("mySiteInfo").avator
-    ? global("mySiteInfo").avator
-    : "../../kiana.png";
-  document.getElementById("userName").innerText = global("mySiteInfo").name ?? "Sakura";
-  document.getElementById("userSite").innerText =
-    global("mySiteInfo").blog ?? "https://tonyteachers.gitee.io/";
+  document.getElementById("userAvator").src = global.avator ? global.avator : "../../kiana.png";
+  document.getElementById("userName").innerText = global.name ?? "Sakura";
+  document.getElementById("userSite").innerText = global.blog ?? "https://tonyteachers.gitee.io/";
 }
 
 function loadView() {
   document.querySelector(`[data-view="${active}"]`).classList.remove("none");
-  console.log(global)
-  // fs.readdir(global.postPath, (err, files) => {
-  //   console.log(files);
-  // });
+  fs.readdir(global.postPath, (err, files) => {
+    console.log(files);
+    postList = files;
+  });
+  InsertPostList();
 }
 
-function closeAside() {
-  aside.style.width = 0;
-  aside.style.padding = 0;
-  aside.style.border = "none";
+function InsertPostList() {
+  console.log(postList)
+  let ul = document.querySelector(".post ul");
+  if (postList.length !== 0) {
+    postList.map((value, index, arr) => {
+      ul.insertAdjacentHTML(
+        "beforeend",
+        `
+    <li>${value.split(".md")[0]}</li>
+    `
+      );
+    });
+  } else {
+    ul.insertAdjacentHTML('afterend',`
+    <div class='not_found_post'>
+    好像没有文章哎=￣ω￣=
+    </div>
+    `)
+  }
 }
 
-function showAside() {
-  aside.style = null;
-}
-
+/**
+ * 绑定选项卡点击切换视图事件
+ */
 document.getElementsByTagName("ul")[0].addEventListener("click", e => {
   let selectId = e.target.dataset.select;
   console.log(selectId);
@@ -49,22 +62,31 @@ document.getElementsByTagName("ul")[0].addEventListener("click", e => {
   document.querySelector(`[data-view="${selectId}"]`).classList.remove("none");
   active = selectId;
 });
-
+/**
+ * 同步站点
+ */
 function syncSite() {
   // exec('npm run start','G:\\Blog',(error,stdout,stderr)=>{
   // })
 }
-
+/**
+ * 在外部浏览器打开博客
+ */
 function openBrowser() {
   shell.openExternal(remote.getGlobal("mySiteInfo").blog);
 }
-
+/**
+ * 点击头像下博客地址自动复制
+ */
 function copySite() {
   let text = document.querySelector("#userSite").innerText;
   clipboard.writeText(text);
   showPopup("已复制到剪贴板");
 }
-
+/**
+ * 复制成功的气泡提示框
+ * @param {string} text 需要显示的文本
+ */
 function showPopup(text) {
   let popup = document.createElement("div");
   popup.innerText = text;
@@ -77,3 +99,12 @@ function showPopup(text) {
 }
 
 init();
+function closeAside() {
+  aside.style.width = 0;
+  aside.style.padding = 0;
+  aside.style.border = "none";
+}
+
+function showAside() {
+  aside.style = null;
+}
